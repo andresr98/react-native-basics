5ecbc4f6843bf9864e462095697f27cd20f9d164
Andres Ruiz Graciano <aandres.r98@gmail.com>
2020-07-28 18:13:22 -0500

    Getting network information with a deprecated NetInfo element

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,7 +5,7 @@ import Dishdetail from './DishdetailComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
 import Login from './LoginComponent';
-import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
+import { View, Platform, Image, StyleSheet, ScrollView, Text, ToastAndroid, NetInfo } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
@@ -286,6 +286,42 @@ class Main extends Component {
         this.props.fetchComments();
         this.props.fetchrPromos();
         this.props.fetchLeaders();
+
+        /*NetInfo.getConnectionInfo()
+            .then((connectionInfo) => {
+                ToastAndroid.show('Initial Network Connectivity Type: ' 
+                    + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType,
+                    ToastAndroid.LONG);
+            });
+
+        NetInfo.addEventListener('connectionChange', this.handleConnectivityChange);*/
+    }
+
+    /*componentWillUnmount() {
+        NetInfo.removeEventListener('connectionChange', this.handleConnectivityChange);
+    }*/
+
+    handleConnectivityChange = (connectionInfo) => {
+        switch(connectionInfo.type) {
+            case 'none':
+                ToastAndroid.show('You are now offline!', ToastAndroid.LONG);
+                break;
+
+            case 'wifi':
+                ToastAndroid.show('You are now connected to Wifi!', ToastAndroid.LONG);
+                break;
+
+            case 'cellular':
+                ToastAndroid.show('You are now connected to Cellular!', ToastAndroid.LONG);
+                break;
+
+            case 'unknown':
+                ToastAndroid.show('You now have an unknown connection!', ToastAndroid.LONG);
+                break;
+
+            default:
+                break
+        }
     }
 
     render() {
