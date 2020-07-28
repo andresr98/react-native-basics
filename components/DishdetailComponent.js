14040b2d4860cb8a662611cb87dfb84995ae97a0
Andres Ruiz Graciano <aandres.r98@gmail.com>
2020-07-28 14:51:46 -0500

    Added a button to share to social medias

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, FlatList, StyleSheet, Modal, Button, Alert, PanResponder } from 'react-native';
+import { View, Text, ScrollView, FlatList, StyleSheet, Modal, 
+    Button, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Input, Rating } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -72,6 +73,16 @@ function RenderDish(props) {
         } 
     });
 
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        }, {
+            dialogTitle: 'Share ' + title
+        });
+    }
+
     if (dish != null) {
         return(
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
@@ -102,6 +113,14 @@ function RenderDish(props) {
                             type='font-awesome'
                             color='#512DA8'
                             onPress={() => props.modal()}
+                            />
+                        <Icon 
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#512DA8'
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)}
                             />
                     </View>
                 </Card>
